194d012c57931d7ab10c3a2cc9db9b7d3fd88d33
Alina087 <alinakildibaeva087@gmail.com>
2026-03-11 09:59:27 +0500

    Обувной магазин (демка)

---
+++ Demo/Demo/Model/StructureOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Demo.Model;
+
+public partial class StructureOrder
+{
+    public int StructureOrderId { get; set; }
+
+    public int? OrderId { get; set; }
+
+    public string? TovarArticle { get; set; }
+
+    public int? StructureOrderTovarCount { get; set; }
+
+    public virtual Order? Order { get; set; }
+
+    public virtual Tovar? TovarArticleNavigation { get; set; }
+}
---
+++ Demo/Demo/Windows/LoginWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using Demo.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (var db = new DbShoes1Context())
+            {
+                var user = db.Users.Include(a => a.Role).FirstOrDefault(a => a.UserLogin == login.Text && a.UserPass == pass.Text);
+                if (user != null)
+                {
+                    App.CurrentUser = user;
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                    this.Close();
+                    
+                }
+                else
+                {
+                    MessageBox.Show("Логин или пароль неверный", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+               
+
+            }
+        }
+    }
+}
